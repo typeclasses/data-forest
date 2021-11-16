af1c5ac90a7656b340b2574ff4be033a68517ca2
Chris Martin <ch.martin@gmail.com>
2021-11-16 15:38:44 -0700

    ghc 9.2

--- .github/workflows/test.hs
+++ .github/workflows/test.hs
@@ -12,6 +12,7 @@ x .= Just y  = Just ("--constraint=" ++ x ++ "==" ++ y)
 x .= Nothing = Nothing
 
 data GHC = GHC_8_0 | GHC_8_2 | GHC_8_4 | GHC_8_6 | GHC_8_8 | GHC_8_10
+         | GHC_9_0 | GHC_9_2
 
 readGHC s = case s of
     "8.0"  -> GHC_8_0
@@ -20,6 +21,8 @@ readGHC s = case s of
     "8.6"  -> GHC_8_6
     "8.8"  -> GHC_8_8
     "8.10" -> GHC_8_10
+    "9.0" -> GHC_9_0
+    "9.2" -> GHC_9_2
 
 constraints ghc = catMaybes
     [ "base" .= case ghc of
@@ -29,5 +32,7 @@ constraints ghc = catMaybes
           GHC_8_6  -> Just "4.12.*"
           GHC_8_8  -> Just "4.13.*"
           GHC_8_10 -> Just "4.14.*"
+          GHC_9_0  -> Just "4.15.*"
+          GHC_9_2  -> Just "4.16.*"
     ]
 
